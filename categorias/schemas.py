51c282df2c50d6b55dc6e388fc661b9bb67f25fe
Ricardo <96831662+rtmazzeo@users.noreply.github.com>
2024-06-28 08:19:38 -0300

    Update class-CategoriaOut

--- categorias/schemas.py
+++ categorias/schemas.py
@@ -6,3 +6,6 @@ from workout_api.contrib.schemas import BaseSchema
 
 class Categoria(BaseSchema):
     nome: Annotated[str, Field(description='Nome da Categoria', exemple='Scale', max_length=10)]
+
+class CategoriaOut(CategoriaIn):
+    id: Annotated[UUID4, Field(description='Identificador da categoria')]
