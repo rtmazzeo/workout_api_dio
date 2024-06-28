4c04e9ef9c72f75c7584799805e7f4093defc90d
Ricardo <96831662+rtmazzeo@users.noreply.github.com>
2024-06-28 08:25:51 -0300

    Update baseschema e CentroTreinamentoOut.py

--- centro_treinamento/schemas.py
+++ centro_treinamento/schemas.py
@@ -7,3 +7,10 @@ class CentroTreinamento(BaseSchema):
     nome: Annotated[str, Field(description='Nome do Centro de Treinamento', exemple='Ct King', max_length=20)]
     endereco: Annotated[str, Field(description='Endereço do Centro de Treinamento', exemple='Rua das Almas, 30', max_length=60)]
     proprietario: Annotated[str, Field(description='Proprietário do Centro de Treinamento', exemple='João Fortão', max_length=30)]
+
+class CentroTreinamentoAtleta(BaseSchema):
+    nome: Annotated[str, Field(description='Nome do centro de treinamento', examples=['CT King'], max_length=20)]
+
+
+class CentroTreinamentoOut(CentroTreinamentoIn):
+    id: Annotated[UUID4, Field(description='Identificador do centro de treinamento')]  
