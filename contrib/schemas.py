30fcb05c009d40de86e3ebbac8fa4d3b64e8f0a0
Ricardo <96831662+rtmazzeo@users.noreply.github.com>
2024-06-28 08:29:42 -0300

    Update schemas - OutMixin.py

--- contrib/schemas.py
+++ contrib/schemas.py
@@ -4,3 +4,7 @@ class BaseSchema(BaseModel):
     class Config:
         extra = 'forbid'
         from_attributes = True
+
+class OutMixin(BaseSchema):
+    id: Annotated[UUID4, Field(description="Identificador")]
+    created_at: Annotated[datetime, Field(description="Data de criação")]
