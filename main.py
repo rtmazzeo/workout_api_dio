f6a7906b134afea9ca2d7ffcb994c630cb7afc10
Ricardo <96831662+rtmazzeo@users.noreply.github.com>
2024-06-28 08:30:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from fastapi import FastAPI
+from workout_api.routers import api_router
 
 app = FastAPI(title='WorkoutApi')
-
-if __name__ == 'main':
-    import uvicorn
-
-    uvicorn.run('main:app', host='0.0.0.0', port=8000, log_level='info',reload=True)
+app.include_router(api_router)
